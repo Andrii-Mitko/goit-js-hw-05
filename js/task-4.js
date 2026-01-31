e875097f598c82db1ed3328e02cc91ed2420cbb2
Andrii-Mitko <andreymit123@gmail.com>
2026-01-31 17:54:57 +0200

    add

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,6 @@
 const getTotalBalanceByGender = (users, gender) => users
-    .filter(user => user.gender.trim() === gender())
-    .reduce((total, user) => total + user.balance, 0);
+    .filter(user => user.gender === gender)
+  .reduce((total, user) => total + user.balance, 0);
 
 
 
